633dc4f0274b3fe99f39858caef82b272720e903
Mihails Butorins <mihails.butorins@gmail.com>
2023-02-24 09:05:51 +0100

    Fix web server test script to run all scenarios at once

--- web-server-test.sh
+++ web-server-test.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 set -m
 
-JAR="${1}"
-
 DIR=`dirname "$0"`
-CMD="perf stat -e 'power/energy-pkg/' --timeout 60000 -- java -jar $JAR ./workload/pets-600.json &"
+JARS=(
+    "./out/akkahttp/2.13.10/assembly.dest/out.jar"
+    "./out/akkahttp/3.2.2/assembly.dest/out.jar"
+    "./out/ziohttp/2.13.10/assembly.dest/out.jar"
+    "./out/ziohttp/3.2.2/assembly.dest/out.jar"
+    "./out/http4s/2.13.10/assembly.dest/out.jar"
+    "./out/http4s/3.2.2/assembly.dest/out.jar"
+)
 
-for APPLY_LOAD in {false,true}; do
-    for RUN_N in {1..3}; do
+for JAR in "${JARS[@]}"; do
+    CMD="perf stat -e 'power/energy-pkg/' --timeout 60000 -- java -jar $JAR $DIR/workload/pets-600.json &"
 
-        if [ "$APPLY_LOAD" == "true" ]; then
-            for LOAD_RATE in {20,40,60}; do
-                printf "Load rate = ${LOAD_RATE}. Run #${RUN_N}:\n\n"
+    for APPLY_LOAD in {false,true}; do
+        for RUN_N in {1..5}; do
+
+            if [ "$APPLY_LOAD" == "true" ]; then
+                printf "$JAR - With load. Run #${RUN_N}:\n\n"
 
                 eval $CMD
 
                 sleep 5
-                wrk2 -R 40 -d 30 -t 4 http://localhost:8080/sustainability-test
+                wrk -t4 -d120 -d30 http://localhost:8080/sustainability-test
 
                 fg %1
-            done
-        else
-            printf "No load. Run #${RUN_N}:\n\n"
+            else
+                printf "$JAR - No load. Run #${RUN_N}:\n\n"
 
-            eval $CMD
-            fg %1
-        fi
+                eval $CMD
+                fg %1
+            fi
+        done
     done
 done
